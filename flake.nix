6fe621ce241d2ed825473f4a3003c38c2020af8f
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-01 22:02:42 +0500

    feat: added hydra and mkFLake

--- flake.nix
+++ flake.nix
@@ -3,34 +3,24 @@
 
   inputs = {
     # Stable for keeping thins clean
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-
-    # Fresh and new for testing
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    # The flake-utils library
-    flake-utils.url = "github:numtide/flake-utils";
+    # # Fresh and new for testing
+    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-unstable";
 
     crane.url = "github:ipetkov/crane";
+
+    xinux-lib = {
+      url = "github:xinux-org/lib/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    crane,
-    ...
-  }:
-  # @ inputs:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      # Nix script formatter
-      formatter = pkgs.alejandra;
-
-      # Development environment
-      devShells.default = import ./shell.nix {inherit pkgs;};
-
-      # Output package
-      packages.default = pkgs.callPackage ./. {inherit pkgs crane;};
-    });
+  outputs =
+    inputs:
+    inputs.xinux-lib.mkFlake {
+      inherit inputs;
+      alias.packages.default = "xinux-settings";
+      alias.shells.default = "xinux-settings";
+      src = ./.;
+      hydraJobs = inputs.self.packages.x86_64-linux;
+    };
 }
