2ab8c5b37ce365f1f777df1e3c6daeb127bc047f
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-03 10:54:28 +0500

    chore: fix spasing and disable ssh page for now

--- src/ui/system/system_page.rs
+++ src/ui/system/system_page.rs
@@ -36,104 +36,103 @@ impl SimpleComponent for SystemPageModel {
     type Output = AppMsg;
 
     view! {
-      #[name = "navigation"]
-      adw::NavigationView {
-          add = &adw::NavigationPage {
-              set_title: "System",
+        #[name = "navigation"]
+        adw::NavigationView {
+            add = &adw::NavigationPage {
+                set_title: "System",
 
-              adw::ToolbarView {
-                  set_top_bar_style: adw::ToolbarStyle::Flat,
+                adw::ToolbarView {
+                    set_top_bar_style: adw::ToolbarStyle::Flat,
+                    add_top_bar = &adw::HeaderBar {},
 
-                  add_top_bar = &adw::HeaderBar {},
+                    adw::PreferencesPage {
+                        adw::PreferencesGroup {
+                            adw::ActionRow {
+                                set_title: "Region and Language",
+                                set_subtitle: "System language and localization",
+                                set_activatable: true,
 
-                  adw::PreferencesPage {
-                      adw::PreferencesGroup {
-                          adw::ActionRow {
-                              set_title: "Region and Language",
-                              set_subtitle: "System language and localization",
-                              set_activatable: true,
+                                add_prefix = &gtk::Image {
+                                    set_icon_name: Some("emoji-flags-symbolic"),
+                                    set_pixel_size: 16
+                                },
 
-                              add_prefix = &gtk::Image {
-                                  set_icon_name: Some("emoji-flags-symbolic"),
-                                  set_pixel_size: 16
-                              },
+                                add_suffix = &gtk::Image {
+                                    set_icon_name: Some("go-next-symbolic"),
+                                    set_pixel_size: 16,
+                                },
 
-                              add_suffix = &gtk::Image {
-                                  set_icon_name: Some("go-next-symbolic"),
-                                  set_pixel_size: 16,
-                              },
+                                connect_activated => SystemPageMsg::OpenSystemRegionLanguagePage,
+                            },
 
-                              connect_activated => SystemPageMsg::OpenSystemRegionLanguagePage,
-                          },
+                            adw::ActionRow {
+                                set_title: "Date and Time",
+                                set_subtitle: "Time zone and clock settings",
+                                set_activatable: true,
 
-                          adw::ActionRow {
-                              set_title: "Date and Time",
-                              set_subtitle: "Time zone and clock settings",
-                              set_activatable: true,
+                                add_prefix = &gtk::Image {
+                                    set_icon_name: Some("preferences-system-time-symbolic"),
+                                    set_pixel_size: 16
+                                },
 
-                              add_prefix = &gtk::Image {
-                                  set_icon_name: Some("preferences-system-time-symbolic"),
-                                  set_pixel_size: 16
-                              },
+                                add_suffix = &gtk::Image {
+                                    set_icon_name: Some("go-next-symbolic"),
+                                    set_pixel_size: 16,
+                                },
 
-                              add_suffix = &gtk::Image {
-                                  set_icon_name: Some("go-next-symbolic"),
-                                  set_pixel_size: 16,
-                              },
+                                connect_activated => SystemPageMsg::OpenSystemDateTimePage
+                            },
 
-                              connect_activated => SystemPageMsg::OpenSystemDateTimePage
-                          },
+                            adw::ActionRow {
+                                set_title: "Users",
+                                set_subtitle: "Add and remove accounts, change password",
+                                set_activatable: true,
 
-                          adw::ActionRow {
-                              set_title: "Users",
-                              set_subtitle: "Add and remove accounts, change password",
-                              set_activatable: true,
+                                add_prefix = &gtk::Image {
+                                    set_icon_name: Some("org.gnome.Settings-users-symbolic"),
+                                    set_pixel_size: 16
+                                },
 
-                              add_prefix = &gtk::Image {
-                                  set_icon_name: Some("org.gnome.Settings-users-symbolic"),
-                                  set_pixel_size: 16
-                              },
+                                add_suffix = &gtk::Image {
+                                    set_icon_name: Some("go-next-symbolic"),
+                                    set_pixel_size: 16,
+                                }
+                            },
 
-                              add_suffix = &gtk::Image {
-                                  set_icon_name: Some("go-next-symbolic"),
-                                  set_pixel_size: 16,
-                              }
-                          },
+                            // adw::ActionRow {
+                            //     set_title: "Secure Shell",
+                            //     set_subtitle: "SSH network access",
+                            //     set_activatable: true,
 
-                          adw::ActionRow {
-                              set_title: "Secure Shell",
-                              set_subtitle: "SSH network access",
-                              set_activatable: true,
+                            //     add_prefix = &gtk::Image {
+                            //         set_icon_name: Some("org.gnome.Settings-secure-shell-symbolic"),
+                            //         set_pixel_size: 16
+                            //     },
 
-                              add_prefix = &gtk::Image {
-                                  set_icon_name: Some("org.gnome.Settings-secure-shell-symbolic"),
-                                  set_pixel_size: 16
-                              },
+                            //     add_suffix = &gtk::Image {
+                            //         set_icon_name: Some("go-next-symbolic"),
+                            //         set_pixel_size: 16,
+                            //     }
+                            // },
 
-                              add_suffix = &gtk::Image {
-                                  set_icon_name: Some("go-next-symbolic"),
-                                  set_pixel_size: 16,
-                              }
-                          },
+                            adw::ActionRow {
+                                set_title: "About",
+                                set_subtitle: "Hardware details and software versions",
+                                set_activatable: true,
 
-                          adw::ActionRow {
-                              set_title: "About",
-                              set_subtitle: "Hardware details and software versions",
-                              set_activatable: true,
+                                add_prefix = &gtk::Image {
+                                    set_icon_name: Some("dialog-warning-symbolicc"),
+                                    set_pixel_size: 16
+                                },
 
-                              add_prefix = &gtk::Image {
-                                  set_icon_name: Some("dialog-warning-symbolicc"),
-                                  set_pixel_size: 16
-                              },
+                                add_suffix = &gtk::Image {
+                                    set_icon_name: Some("go-next-symbolic"),
+                                    set_pixel_size: 16,
+                                },
 
-                              add_suffix = &gtk::Image {
-                                  set_icon_name: Some("go-next-symbolic"),
-                                  set_pixel_size: 16,
-                              },
-
-                              connect_activated => SystemPageMsg::OpenSystemAboutPage,
-                          },
-                      },
+                                connect_activated => SystemPageMsg::OpenSystemAboutPage,
+                            },
+                        },
                   }
               }
           }
