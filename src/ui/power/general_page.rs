aaca723380f1b0791d5cccb702a3e8d75bd65b88
akmal <isakulovdev@gmail.com>
2026-04-09 15:38:27 +0500

    chore: component duplicate

--- src/ui/power/general_page.rs
+++ src/ui/power/general_page.rs
@@ -37,6 +37,21 @@ pub struct GeneralPowerPageView {
 
     #[tracker::do_not_track]
     pub ppd: Arc<PpdProxyBlocking<'static>>,
+
+    // Power Saving Options
+    /// Dim screen
+    pub idle_dim: bool,
+    /// Automatic Screen Black (uint32 0)
+    /// Custom for ComboRow
+    pub auto_screen_black: bool,
+    /// Automatic Screen Black (uint32 0)
+    pub idle_delay: String,
+
+    // Automatic Suspend
+    /// While plugged in (ac => Alternating Current)
+    pub sleep_inactive_ac_type: bool,
+    /// Suspend on AC timeout
+    pub sleep_inactive_ac_timeout: u16,
 }
 
 impl fmt::Display for PowerMode {
@@ -56,6 +71,11 @@ pub enum GeneralPowerPageViewMsg {
     SetChargingMode(ChargingMode),
     ToggleBatteryPercentage(bool),
     SelectPowerButtonAction(usize),
+
+    // Automatic Suspend
+    SetIdleDim(bool),
+    SetSleepInactiveACType(bool),
+    SetAutoScreenBlack(bool),
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -232,7 +252,121 @@ impl Component for GeneralPowerPageView {
                     },
                 },
             },
-        },
+
+            adw::PreferencesGroup {
+                set_title: "Power Saving",
+
+                set_visible: !model.show_batteries,
+
+
+                adw::PreferencesGroup {
+                    adw::ActionRow {
+                        set_title: "Dim Screen",
+                        set_subtitle: "Reduce screen brightness when the device is inactive",
+
+                        add_suffix = &gtk::Switch {
+                            set_valign: gtk::Align::Center,
+                            #[watch]
+                            set_active: model.idle_dim,
+                            connect_state_set[sender] => move |_, state| {
+                                sender.input(GeneralPowerPageViewMsg::SetIdleDim(state));
+                                gtk::glib::Propagation::Proceed
+                            },
+                        },
+                    },
+                },
+            },
+
+            adw::PreferencesGroup {
+                set_visible: !model.show_batteries,
+
+                adw::ActionRow {
+                    set_title: "Automatic Screen Blank",
+                    set_subtitle: "Turn the screen off after a period of inactivity",
+
+                    add_suffix = &gtk::Switch {
+                        set_valign: gtk::Align::Center,
+                        #[watch]
+                        set_active: model.auto_screen_black,
+                        connect_state_set[sender] => move |_, state| {
+                            sender.input(GeneralPowerPageViewMsg::SetAutoScreenBlack(state));
+                            gtk::glib::Propagation::Proceed
+                        },
+                    },
+                },
+
+                adw::ComboRow {
+                    #[watch]
+                    set_sensitive: model.auto_screen_black,
+
+                    set_title: "Delay",
+                    set_model: Some(&gtk::StringList::new(&[
+                        "1 minute",
+                        "2 minute",
+                        "3 minute",
+                        "4 minute",
+                        "5 minute",
+                        "8 minute",
+                        "10 minute",
+                        "12 minute",
+                        "15 minute",
+                    ])),
+                }
+            },
+
+            adw::PreferencesGroup {
+                set_visible: !model.show_batteries,
+
+                adw::ActionRow {
+                    set_title: "Automatic Suspend",
+
+                    add_suffix = &gtk::Switch {
+                        set_valign: gtk::Align::Center,
+                        #[watch]
+                        set_active: model.sleep_inactive_ac_type,
+                        connect_state_set[sender] => move |_, state| {
+                            sender.input(GeneralPowerPageViewMsg::SetSleepInactiveACType(state));
+                            gtk::glib::Propagation::Proceed
+                        },
+                    },
+                },
+
+                adw::ComboRow {
+                    #[watch]
+                    set_sensitive: model.sleep_inactive_ac_type,
+
+                    set_title: "Delay",
+                    set_model: Some(&gtk::StringList::new(&[
+                        "15 minute",
+                        "20 minute",
+                        "25 minute",
+                        "30 minute",
+                        "45 minute",
+                        "1 hour",
+                        "1 hour 20 minute",
+                        "1 hour 30 minute",
+                        "1 hour 40 minute",
+                        "2 hours",
+                    ])),
+                }
+            },
+
+            adw::PreferencesGroup {
+                set_visible: !model.show_batteries,
+
+                adw::ActionRow {
+                    set_title: "Disabling automatic suspend will result in higher power consumption. It is recomended to keep automatic suspend enabled.",
+
+                    #[watch]
+                    set_visible: !model.sleep_inactive_ac_type,
+
+                    add_prefix = &gtk::Image {
+                        set_icon_name: Some("info-outline"),
+                        set_pixel_size: 24,
+                    }
+                }
+            }
+        }
     }
 
     fn init(
@@ -293,6 +427,28 @@ impl Component for GeneralPowerPageView {
 
             ppd: Arc::new(proxy),
             tracker: 0,
+
+            // In case there is no battery
+            idle_dim: dconf_rs::get_boolean("/org/gnome/settings-daemon/plugins/power/idle-dim")
+                .unwrap(),
+
+            auto_screen_black: true,
+
+            idle_delay: dconf_rs::get_string("/org/gnome/desktop/session/idle-delay").unwrap(),
+
+            sleep_inactive_ac_type: matches!(
+                (dconf_rs::get_string(
+                    "/org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-type",
+                )
+                .unwrap()
+                .as_str(),),
+                ("suspend",)
+            ),
+
+            sleep_inactive_ac_timeout: dconf_rs::get_int(
+                "/org/gnome/settings-daemon/plugins/power/sleep-inactive-battery-timeout",
+            )
+            .unwrap() as u16,
         };
 
         let combo_row = model.power_button_action_row.widget();
@@ -341,6 +497,37 @@ impl Component for GeneralPowerPageView {
                     ChargingMode::Maximize => change_battery_threshold(20, 100),
                     ChargingMode::Unsupported => change_battery_threshold(20, 100),
                 }
+            }
+
+            GeneralPowerPageViewMsg::SetIdleDim(state) => {
+                self.idle_dim = state;
+
+                let _ = dconf_rs::set_boolean(
+                    "/org/gnome/settings-daemon/plugins/power/idle-dim",
+                    state,
+                );
+            }
+
+            GeneralPowerPageViewMsg::SetSleepInactiveACType(state) => match state {
+                true => {
+                    self.sleep_inactive_ac_type = state;
+
+                    let _ = dconf_rs::set_string(
+                        "/org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-type",
+                        "suspend",
+                    );
+                }
+                false => {
+                    self.sleep_inactive_ac_type = state;
+
+                    let _ = dconf_rs::set_string(
+                        "/org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-type",
+                        "nothing",
+                    );
+                }
+            },
+            GeneralPowerPageViewMsg::SetAutoScreenBlack(state) => {
+                self.auto_screen_black = state;
             }
         }
     }
