389e18839ffdbe96c59ac96d2ec0f5571784db91
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-17 19:29:28 +0500

    chore: wip user profile page

--- src/ui/system/system_users.rs
+++ src/ui/system/system_users.rs
@@ -1,4 +1,5 @@
 use crate::ui::system::system_page::SystemPageMsg;
+use adw::{ActionRow, gtk::ListBoxRow};
 use gettextrs::gettext;
 use relm4::{
     adw::{self, prelude::*},
@@ -45,6 +46,62 @@ impl SimpleComponent for SystemUsersPage {
                         adw::PreferencesPage {
                             // #[local_ref]
                             // current_user -> adw::PreferencesGroup {},
+                            adw::PreferencesGroup {
+                                gtk::Overlay {
+                                    set_halign: gtk::Align::Center,
+                                    // set_sensitive: bind template.avatar-editable;
+                                    // set_has_tooltip: bind template.avatar-editable no-sync-create inverted;
+                                    set_tooltip_text: Some(&gettext("Unlock to Change This Setting")),
+
+                                    #[name(avatar)]
+                                    adw::Avatar {
+                                        set_show_initials: true,
+                                        set_size: 120,
+                                        set_halign: gtk::Align::Center,
+                                    },
+
+                                    // [overlay]
+                                    adw::Bin {
+                                        add_css_class: "cutout-button",
+
+                                        set_halign: gtk::Align::End,
+                                        set_valign: gtk::Align::End,
+
+                                        #[name(avatar_edit_button)]
+                                        gtk::MenuButton {
+                                            set_tooltip_text: Some(&gettext("Change Avatar")),
+                                            set_icon_name: "document-edit-symbolic",
+                                            add_css_class: "circular",
+                                        }
+                                    }
+                                }
+                            },
+
+                            adw::PreferencesGroup {
+                                #[name(fullname_row)]
+                                adw::EntryRow {
+                                    // sensitive: bind template.editable;
+                                    // has-tooltip: bind template.editable no-sync-create inverted;
+                                    set_tooltip_text: Some(&gettext("Unlock to Change This Setting")),
+                                    set_input_purpose: gtk::InputPurpose::Name,
+                                    set_show_apply_button: true,
+                                    set_title: &gettext("Name"),
+                                    set_use_underline: true,
+                                    // apply => $fullname_entry_apply_cb(template);
+                                },
+
+                                #[name(password_row)]
+                                adw::ActionRow {
+                                    // sensitive: bind template.editable;
+                                    // set_has_tooltip: bind template.editable no-sync-create inverted;
+                                    set_tooltip_text: Some(&gettext("Unlock to Change This Setting")),
+                                    set_activatable: true,
+                                    set_title: &gettext("Password"),
+                                    set_use_underline: true,
+                                    // set_show_arrow: true,
+                                    // set_activated => $change_password(template);
+                                },
+                            },
 
                             adw::PreferencesGroup {
                                 #[watch]
