3f28eeb830db856f14de2fd952b991cb36eaa334
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-20 18:37:54 +0500

    refactor: view appearance

--- src/ui/appearance/appearance.rs
+++ src/ui/appearance/appearance.rs
@@ -148,7 +148,7 @@ impl AsyncComponent for AppearanceModel {
         #[root]
         adw::BreakpointBin {
             set_height_request: 200,
-            set_width_request: 346,
+            set_width_request: 200,
 
             add_breakpoint = adw::Breakpoint::new(
                 adw::BreakpointCondition::new_length(
@@ -339,7 +339,6 @@ impl AsyncComponent for AppearanceModel {
                             adw::Bin {
                                 add_css_class: "card",
                                 set_accessible_role: gtk::AccessibleRole::Group,
-                                // set_toast_overlay: toast_overlay,
 
                                 #[name = "background_chooser"]
                                 gtk::Box {
@@ -366,15 +365,11 @@ impl AsyncComponent for AppearanceModel {
                                             set_activate_on_single_click: true,
                                             set_selection_mode: gtk::SelectionMode::Single
                                         },
-
                                         gtk::Separator {
                                             set_margin_top: 12,
                                             set_margin_bottom: 12,
                                         }
                                     },
-                                    ////////////////////////////////////////////////////////////
-
-                                    // #[name = "flowbox"]
                                     #[local_ref]
                                     wallpaper_box -> gtk::FlowBox {
                                         add_css_class: "background-flowbox",
@@ -387,12 +382,6 @@ impl AsyncComponent for AppearanceModel {
                                         set_max_children_per_line: 3,
                                         set_activate_on_single_click: true,
                                         set_selection_mode: gtk::SelectionMode::Single,
-
-
-                                        #[name="wallpaper_group"]
-                                        gtk::ToggleButton {
-                                            // set_group: Some(&wallpaper_group),
-                                        },
                                     },
                                 },
                             },
