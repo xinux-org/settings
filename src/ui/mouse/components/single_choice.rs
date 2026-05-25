f56718a5101f1b639ca8ca87056c631964faa81a
akmal <isakulovdev@gmail.com>
2026-05-25 15:49:50 +0500

    fix: animation position on `Tap to Click` component

--- src/ui/mouse/components/single_choice.rs
+++ src/ui/mouse/components/single_choice.rs
@@ -1,4 +1,4 @@
-use adw::glib::{Variant, property::PropertyGet};
+use adw::glib::Variant;
 use gtk::gio::Settings;
 use relm4::{
     adw::prelude::*,
@@ -50,19 +50,59 @@ impl SimpleComponent for SingleChoice {
     type Output = SingleChoiceOutput;
 
     view! {
-        adw::ActionRow {
-            add_prefix = &gtk::Label {
-              set_label: model.row_option.title.as_str(),  
+        adw::PreferencesRow {
+            set_activatable: false,
+
+            add_controller = gtk::GestureClick {
+                connect_pressed[sender, enable_row_option] => move |_,_,_,_| {
+                    let enabled = enable_row_option.is_active();   
+                    let _ = sender.input_sender().send(SingleChoiceMsg::Switch(!enabled));
+                },
             },
 
-            add_suffix = &gtk::Box {
-                set_orientation: gtk::Orientation::Horizontal,
-                set_spacing: 12,
-                set_hexpand: true,
+            #[wrap(Some)]
+            set_child = &gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_hexpand: false,
+                
 
-                append = &gtk::Box {
-                    set_orientation: gtk::Orientation::Vertical,
-                    set_spacing: 6,
+                #[name(header)]
+                gtk::Box {
+                    set_valign: gtk::Align::Center,
+
+                    #[name(prefixes)]
+                    gtk::Box {
+                      set_visible: false,
+                    },
+
+                    #[name(title_box)]
+                    gtk::Box {
+                        set_orientation: gtk::Orientation::Vertical,
+                        set_valign: gtk::Align::Center,
+                        set_hexpand: true,
+
+                        #[name(title)]
+                        gtk::Label {
+                            set_margin_all: 16,
+                            set_label: model.row_option.title.as_str(),
+                            set_align: gtk::Align::Start,
+                        },
+
+                        #[name(subtitle)]
+                        gtk::Label {
+                            set_xalign: 0.06,
+                            set_wrap: true,
+                            set_wrap_mode: pango::WrapMode::WordChar,
+                            set_label: model.row_option.subtitle.as_str(),
+                            add_css_class: "subtitle",
+                        },
+                    },
+
+                    #[name(suffixes)]
+                    gtk::Box {
+                        set_visible: false,
+                        add_css_class: "suffixes",
+                    },
 
                     #[name(enable_row_option)]
                     append = &adw::SwitchRow {
@@ -72,37 +112,63 @@ impl SimpleComponent for SingleChoice {
                             sender.input(SingleChoiceMsg::Switch(btn.is_active()));
                         },
                     },
+                },
 
-                    add_controller = gtk::EventControllerMotion {
-                        connect_enter[sender] => move |_,_,_| {
-                            let _ = sender.input_sender().send(SingleChoiceMsg::MediaPlay(true));
+                #[name(contents)]
+                gtk::Box {
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_hexpand: true,
+                    set_homogeneous: true,
+                    set_spacing: 3,
+
+                    #[name(default_option_box)]
+                    gtk::Box {
+                        set_accessible_role: gtk::AccessibleRole::Radio,
+                        set_orientation: gtk::Orientation::Vertical,
+                        set_can_focus: true,
+                        set_focus_on_click: false,
+                        set_focusable: false,
+                        set_receives_default: true,
+
+                        #[iterate]
+                        add_css_class: ["activatable"],
+
+                        adw::Bin {
+                            set_margin_top: 9,
+                            set_margin_bottom: 9,
+                            set_margin_start: 9,
+                            set_margin_end: 9,
+
+                            #[iterate]
+                            add_css_class: ["background","frame"],
+
+                            add_controller = gtk::EventControllerMotion {
+                                connect_enter[sender] => move |_,_,_| {
+                                    let _ = sender.input_sender().send(SingleChoiceMsg::MediaPlay(true));
+                                },
+
+                                connect_leave[sender] => move |_| {
+                                    let _ = sender.input_sender().send(SingleChoiceMsg::MediaPlay(false));
+                                },
+                            },
+
+                            #[name(default_option_picture)]
+                            gtk::Picture {
+                                set_hexpand: true,
+                                set_halign: gtk::Align::Center,
+                                set_margin_top: 6,
+                                set_margin_bottom: 6,
+                                set_margin_start: 6,
+                                set_margin_end: 6,
+                                set_height_request: 128,
+
+                                set_paintable: Some(&model.row_option.media),
+                            },
                         },
-
-                        connect_leave[sender] => move |_| {
-                            let _ = sender.input_sender().send(SingleChoiceMsg::MediaPlay(false));
-                        },
-                    },
-
-                    add_controller = gtk::GestureClick {
-                        connect_pressed[sender, enable_row_option] => move |_,_,_,_| {
-                            let enabled = enable_row_option.is_active();   
-                            let _ = sender.input_sender().send(SingleChoiceMsg::Switch(!enabled));
-                        },
-                    },
-
-                    append = &gtk::Picture {
-                        set_hexpand: true,
-                        set_halign: gtk::Align::Center,
-                        set_margin_top: 6,
-                        set_margin_bottom: 6,
-                        set_margin_start: 6,
-                        set_margin_end: 6,
-                        set_height_request: 128,
-
-                        set_paintable: Some(&model.row_option.media),
                     },
                 },
-            }
+            },
+
         },
     }
 
