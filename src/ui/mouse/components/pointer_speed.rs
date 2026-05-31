e1714aeb14fe2b8cf7906e1029ae3258086a366a
akmal <isakulovdev@gmail.com>
2026-05-31 19:33:11 +0500

    fix: pointer speed reinitialization in `gtk::Adjustment`
    Fixes: #32

--- src/ui/mouse/components/pointer_speed.rs
+++ src/ui/mouse/components/pointer_speed.rs
@@ -9,7 +9,6 @@ use relm4::{
 pub struct PointerSpeed {
     settings: Settings,
     speed: f64,
-    mouse_speed_scale: Option<gtk::Scale>,
 }
 
 #[derive(Debug)]
@@ -38,11 +37,10 @@ impl Component for PointerSpeed {
         adw::ActionRow {
             set_title: "Pointer Speed",
             set_use_underline: true,
-            // set_activatable_widget: model.mouse_speed_scale,
+
             #[name(mouse_speed_scale)]
             add_suffix = &gtk::Scale {
                 set_hexpand: true,
-                set_value: model.speed,
                 set_range: (-1.0, 1.0),
                 add_mark: (-1.0, gtk::PositionType::Bottom, Some("Slow")),
                 add_mark: (0.0, gtk::PositionType::Bottom, None),
@@ -52,7 +50,10 @@ impl Component for PointerSpeed {
                     set_upper: 1.0,
                     set_step_increment: 0.1,
                     set_page_increment: 0.1,
+
+                    set_value: model.speed,
                 },
+
                 connect_value_changed[sender] => move |scale|{
                     sender.input(PointerSpeedMsg::PointerSpeed(scale.value()));
                 }
@@ -65,15 +66,12 @@ impl Component for PointerSpeed {
         root: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let mut model = Self {
+        let model = Self {
             settings: init.settings,
             speed: init.speed,
-            mouse_speed_scale: None,
         };
 
         let widgets = view_output!();
-        let mouse_speed_scale = widgets.mouse_speed_scale.clone();
-        model.mouse_speed_scale = Some(mouse_speed_scale);
 
         ComponentParts { model, widgets }
     }
