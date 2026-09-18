a39823c0387b8a0334248eda78336faa7eb319e7
Ezozbek <git@ezozbek.dev>
2026-09-18 12:59:07 +0500

    feat(display): move ui folder

--- src/ui/display/mod.rs
+++ src/ui/display/mod.rs
@@ -1,6 +1,10 @@
+pub use display_page::*;
+pub use display_panel::*;
+pub use display_settings::*;
 pub use manager::*;
-pub use ui::*;
 
 mod dbus;
+mod display_page;
+mod display_panel;
+mod display_settings;
 mod manager;
-mod ui;
--- src/ui/display/ui/mod.rs
+++
@@ -1,7 +0,0 @@
-pub use display_page::*;
-pub use display_panel::*;
-pub use display_settings::*;
-
-mod display_page;
-mod display_panel;
-mod display_settings;
